ed32090be7fec7d182d81bc11dc639812b517933
Alexis Langille <langille.alexis@gmail.com>
2024-08-03 12:36:54 -0400

    Add Options to Add Role and Employee

--- index.js
+++ index.js
@@ -52,8 +52,57 @@ const mainMenu = async () => {
             console.log(`Added ${name} to the database.`);
             break;
         case 'Add a role':
+            const departmentsData = await viewAllDepartments();
+            const { title, salary, departmentId } = await prompt([
+                {
+                    name: 'title',
+                    type: 'input',
+                    message: 'Enter the name of the role:'
+                },
+                {
+                    name: 'salary',
+                    type: 'input',
+                    message: 'Enter the salary of the role:'
+                },
+                {
+                    name: 'departmentId',
+                    type: 'list',
+                    message: 'Select the department for this role:',
+                    choices: departmentsData.map(department => ({ name: department.name, value: department.id }))
+                }
+            ]);
+            await addRole(title, salary, departmentId);
+            console.log(`Added ${title} to the database.`);
             break;
         case 'Add an employee':
+            const rolesData = await viewAllRoles();
+            const employeesData = await viewAllEmployees();
+            const { firstName, lastName, roleId, managerId } = await prompt([
+                {
+                    name: 'firstName',
+                    type: 'input',
+                    message: 'Enter the first name of the employee:'
+                },
+                {
+                    name: 'lastName',
+                    type: 'input',
+                    message: 'Enter the last name of the employee:'
+                },
+                {
+                    name: 'roleId',
+                    type: 'list',
+                    message: 'Select the role for this employee:',
+                    choices: rolesData.map(role => ({ name: role.title, value: role.id }))
+                },
+                {
+                    name: 'managerId',
+                    type: 'list',
+                    message: 'Select the manager for this employee:',
+                    choices: [{ name: 'None', value: null }, ...employeesData.map(employee => ({ name: `${employee.first_name} ${employee.last_name}`, value: employee.id }))]
+                }
+            ]);
+            await addEmployee(firstName, lastName, roleId, managerId);
+            console.log(`Added ${firstName} ${lastName} to the database.`);
             break;
         case 'Update an employee role':
             break;
