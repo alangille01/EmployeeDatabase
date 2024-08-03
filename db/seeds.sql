a5142116efd88797407883cf3d77c8781b377e8c
Alexis Langille <langille.alexis@gmail.com>
2024-08-03 12:26:03 -0400

    Add Data into Tables
    This adds mock data into the employee, role and department tables.

--- db/seeds.sql
+++ db/seeds.sql
@@ -22,3 +22,15 @@ CREATE TABLE employee (
     role_id INTEGER NOT NULL REFERENCES role(id),
     manager_id INTEGER REFERENCES employee(id)
 );
+
+INSERT INTO department (name) VALUES ('Engineering'), ('HR'), ('Sales');
+
+INSERT INTO role (title, salary, department_id) VALUES 
+('Software Engineer', 80000, 1), 
+('HR Manager', 60000, 2), 
+('Sales Executive', 70000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
+('John', 'Doe', 1, NULL), 
+('Jane', 'Smith', 2, 1), 
+('Alexis', 'Langille', 3, 1);
